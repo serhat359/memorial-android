fcad3247c1cc20a01eae053613ce89744a8e53a3
Serhat Abdulbakioğlu <serhat359@gmail.com>
2016-05-28 13:16:09 +0300

    assets altından dosya okuma yapıldı debug view oluşturuldu

--- src/com/example/memorialandroid/MainActivity.java
+++ src/com/example/memorialandroid/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.memorialandroid;
 
 import android.support.v4.app.FragmentActivity;
+
+import java.io.*;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity{
 
+	static TextView debugView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Set Debug
+		debugView = (TextView)findViewById(R.id.debug);
+		
+		// Actual Code Start
+		String sampleText = getTextOfSample("sample.txt");
+		
+		debug(sampleText);
 	}
 
 	@Override
@@ -30,5 +44,38 @@ public class MainActivity extends FragmentActivity{
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private static void debug(String message){
+		debugView.setText(message);
+	}
+	
+	private String getTextOfSample(String fileName){
+		try{
+			InputStream fis = getResources().getAssets().open(fileName);
+
+			String text = readStream(fis);
+			
+			return text;
+		}
+		catch(IOException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return e.getMessage();
+		}
+	}
+	
+	private static String readStream(InputStream is) {
+	    StringBuilder sb = new StringBuilder(512);
+	    try {
+	        Reader r = new InputStreamReader(is, "UTF-8");
+	        int c = 0;
+	        while ((c = r.read()) != -1) {
+	            sb.append((char) c);
+	        }
+	    } catch (IOException e) {
+	        throw new RuntimeException(e);
+	    }
+	    return sb.toString();
 	}
 }

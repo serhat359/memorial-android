1af6680de1cccdfbca56e53047644b90f981c191
Serhat Abdulbakioğlu <serhat359@gmail.com>
2016-07-08 18:49:48 +0300

    refactor

--- src/com/example/memorialandroid/MainActivity.java
+++ src/com/example/memorialandroid/MainActivity.java
@@ -62,7 +62,6 @@ public class MainActivity extends FragmentActivity implements Debugable{
 	public boolean onOptionsItemSelected(MenuItem item){
 		switch(item.getItemId()){
 			case R.id.action_settings:
-				// User chose the "Settings" item, show the app settings UI...
 				return true;
 
 			case R.id.update:
@@ -76,10 +75,7 @@ public class MainActivity extends FragmentActivity implements Debugable{
 				return true;
 
 			default:
-				// If we got here, the user's action was not recognized.
-				// Invoke the superclass to handle it.
 				return super.onOptionsItemSelected(item);
-
 		}
 	}
 
